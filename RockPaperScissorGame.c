572ba53661b91abeaa22deebf744995f6ea8c717
Deepak <deepak@gmail.com>
2024-08-04 19:31:30 +0530

    Rock Paper Scissor game completed

---
+++ RockPaperScissorGame.c
@@ -0,0 +1,98 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main()
+{
+    int choice,c_chose,win=0,tie=0;
+    printf("Welcome to RPS Game\n");
+    printf("-------------------\n");
+    printf("ROCK PAPAER SCISSOR\n");
+    printf("~~~~~~~~~~~~~~~~~~~\n");
+    printf("Type 0 for ROCK\nType 1 for PAPER\nType 2 for SCISSOR\n");
+    for (int  i = 0; i < 5; i++)
+    {
+        printf("Enter your choice:");
+        scanf("%d", &choice);
+        if (choice > 2 || choice < 0)
+        {
+            printf("Hey fool its an invalid number !!\n");
+            i--;
+        }
+        else
+        {
+            srand(time(0));
+            c_chose = rand()%3;
+            printf("Computer chose:%d\n",c_chose);
+            if (c_chose == choice)
+            {
+                printf("DRAW\n");
+                tie++;
+            }
+            else if(c_chose == 0 || choice == 2)
+            {
+                printf("You lose\n");
+            }
+            else if (c_chose == 2 || choice == 0)
+            {
+                printf("You win\n");
+                win++;
+            }
+            else if(c_chose > choice)
+            {
+                printf("You lose\n");
+            }
+            else if(c_chose > choice)
+            {
+                printf("You win\n");
+                win++;
+            }
+        }
+    }
+    printf("You won %d times\n",win);
+    printf("You lose %d times\n",-(win+tie-5));
+    if (win > (-(win+tie-5)))
+    {
+        printf("-----------------\n");
+        printf("YOU ARE THE WINNER");
+    }
+    else if (win == (-(win+tie-5)))
+    {
+        printf("WELL TRIED MATCH TIED");
+    }
+    else
+    {
+        printf("-----------------\n");
+        printf("YOU LOSE");
+    }
+}
+
+/*
+      OUTPUT
+
+Welcome to RPS Game
+-------------------
+ROCK PAPAER SCISSOR
+~~~~~~~~~~~~~~~~~~~
+Type 0 for ROCK
+Type 1 for PAPER
+Type 2 for SCISSOR
+Enter your choice:1
+Computer chose:2
+You win
+Enter your choice:0
+Computer chose:0
+DRAW
+Enter your choice:2
+Computer chose:1
+You lose
+Enter your choice:1
+Computer chose:1
+DRAW
+Enter your choice:0
+Computer chose:2
+You win
+You won 2 times
+You lose 1 times
+-----------------
+YOU ARE THE WINNER
+*/
